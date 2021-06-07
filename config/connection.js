df33746dff0d93901ddcd90aff18e2ae94a8cc0a
Taylor Wright <tjwri25@gmail.com>
2021-06-07 07:48:59 -0400

    deploy

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.JAWSDB_BRONZE_URL
+  ? new Sequelize(process.env.JAWSDB_BRONZE_URL)
   : new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
